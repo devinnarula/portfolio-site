41893b7ff99f39535269510eff3c31c36a20b5ab
Devin <Devin.j.narula@gmail.com>
2020-06-17 21:23:41 -0400

    Contact form

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Navbar, Nav } from 'react-bootstrap';
 import './header.css'
 
 const Header = () => {
     return (
-        // <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-        //   <Nav className="mr-auto">
-        //     <Link className='App-link' to='/'>Home</Link>
-        //     <Link className='App-link' to='/projects'>Projects</Link>
-        //     <Link className='App-link' to='/contact'>Contact</Link>
-        //   </Nav>
-        // </Navbar>
         <header className='Header'>
             <h1 className='Header-Title'>Devin Narula</h1>
             <nav className='Header-Navbar'>
@@ -27,13 +19,13 @@ const Header = () => {
                         </Link>
                     </li>
                     <li>
-                        <Link className='Header-NavItem' activeClassName='Header-ActiveNavItem' to='/Resume.pdf'>
-                            Resume
+                        <Link className='Header-NavItem' activeClassName='Header-ActiveNavItem' to='/contact'>
+                            Contact
                         </Link>
                     </li>
                     <li>
-                        <Link className='Header-NavItem' activeClassName='Header-ActiveNavItem' to='/contact'>
-                            Contact
+                        <Link className='Header-NavItem' activeClassName='Header-ActiveNavItem' href='https://drive.google.com/file/d/1gnTbWk2aFYIm_Yx72NZksM1M-a-pd-rL/view?usp=sharing' target='_blank'>
+                            Resume
                         </Link>
                     </li>
                 </ul>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React, {useState} from 'react';
+import Layout from '../components/layout';
+import './contact.css';
 
 const Contact = () => {
-    
+    let [subject, setSubject] = useState("")
+    let [message, setMessage] = useState("")
+
+    const handleSubjectChange = (e) => {
+        setSubject(e.target.value)
+    }
+    const handleMessageChange = (e) => {
+        setMessage(e.target.value)
+    }
+
     return (
         <Layout>
-            
+            <form className="Contact-Form">
+                <caption className="Contact-Caption">Subject:</caption>
+                <textarea className="Contact-Subject" value={subject} onChange={handleSubjectChange}></textarea>
+                <caption className="Contact-Caption">Message:</caption>
+                <textarea className="Contact-Message" value={message} onChange={handleMessageChange}></textarea>
+                <a className="Contact-Button" href={`mailto:devin.j.narula@gmail.com?&subject=${subject}&body=${message}`}>Send</a>
+            </form>
         </Layout> 
     )
 }
